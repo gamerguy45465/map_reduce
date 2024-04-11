1bfb2a6b2dfa9e92162e9b8991d884ae30308979
root <root@DESKTOP-POS43QA>
2024-04-11 16:39:23 -0600

    Implemented openDatabase

--- database.go
+++ database.go
@@ -3,7 +3,19 @@ package main
 import "database/sql"
 
 func openDatabase(path string) (*sql.DB, error) {
-
+	// the path to the database--this could be an absolute path
+	options :=
+		"?" + "_busy_timeout=10000" +
+			"&" + "_case_sensitive_like=OFF" +
+			"&" + "_foreign_keys=ON" +
+			"&" + "_journal_mode=OFF" +
+			"&" + "_locking_mode=NORMAL" +
+			"&" + "mode=rw" +
+			"&" + "_synchronous=OFF"
+	db, err := sql.Open("sqlite3", path+options)
+	if err != nil {
+		// handle the error here
+	} 
 }
 
 func createDatabase(path string) (*sql.DB, error) {
