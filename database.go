2f87b54ac2434bbbd44b273267dd318cc08e4d37
coderguysomething <jordancoleman370@gmail.com>
2024-04-11 16:23:38 -0600

    Added first functions to database.go and added austen.db

---
+++ database.go
@@ -0,0 +1,31 @@
+package main
+
+import "database/sql"
+
+func openDatabase(path string) (*sql.DB, error) {
+
+}
+
+func createDatabase(path string) (*sql.DB, error) {
+
+}
+
+func splitDataBase(source, paths []string) error {
+
+}
+
+func mergeDatabases(urls []string, path string, temp string) (*sql.DB, error) {
+
+}
+
+func download(url, path string) error {
+
+}
+
+func gatherInfo(db *sql.DB, path string) error {
+
+}
+
+func main() {
+
+}
